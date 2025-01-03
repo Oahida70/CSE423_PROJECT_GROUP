f24fe5bfdf0b111e4e18c4f88e4bc4d245e319a4
Oahida70 <syada.oahida.yesmin@g.bracu.ac.bd>
2025-01-04 00:44:30 +0600

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,14 +1,41 @@
-car_transition_width_corrector_ = 30
-car_transition_height_corrector_ = 0
-screen_width_ , screen_height_ = 850 , 550
-gravels_ = []
-start_game_ = True
-car_velo_y_ = 0
-car_jump_upward_ = False
-tree_speed_game_ = 5
-score_game_ = 0
-start_right_game = 755
-start_left_ = 745
-illusion_game_ = True
-cloud_shifting_r_ = False
-shifting_magnitude_ = 0
+vehicle_trans_width = 30
+
+vehicle_trans_height = 0
+
+
+WIDTH = 800
+HEIGHT = 500
+
+particles = []
+
+score_points = 0
+
+vehicle_velocity = 0
+
+jump_mechanism = False
+
+gamestart = True
+
+obstacle_speed = 4  # tree
+
+initial_position_right = 700
+
+initial_position_left = 745
+
+is_visual_effect_enabled = True
+
+cloud_movement = False
+
+cloud_shift_amount = 0
+
+speed_of_power = 2
+
+background_color = [0.0, 0.0, 0.0, 1.0]
+
+ball_x, ball_y = 100, 200  
+
+ball_radius = 10
+
+ball_velocity_x, ball_velocity_y = 3, -2  
+
+powerup_score = 0
