8bce543d8306a3014823422338bc86a35b819d69
Oahida70 <syada.oahida.yesmin@g.bracu.ac.bd>
2025-01-04 00:51:45 +0600

    Update car.py

--- car.py
+++ car.py
@@ -1,12 +1,25 @@
-c_l_l_x=47
-c_l_l_y=100
-c_u_l_x=47
-c_u_l_y=135
-c_u_r_x=108
-c_u_r_y=135
-c_l_r_x=108
-c_l_r_y=100
-t_l_l_x,t_l_l_y=719,100
-t_u_l_x,t_u_l_y=719,178
-t_u_r_x,t_u_r_y=774,178
-t_l_r_x,t_l_r_y=774,100 
+car_bottom_left_x = 47
+car_bottom_left_y = 100
+car_bottom_right_x = 108
+car_bottom_right_y = 100
+car_top_left_x = 47
+car_top_left_y = 135
+car_top_right_x = 108
+car_top_right_y = 135
+
+def vehicle():
+    global vehicle_trans_width, vehicle_trans_height
+
+    draw_line_mpl(20 + vehicle_trans_width, 110 + vehicle_trans_height, 75 + vehicle_trans_width, 110 + vehicle_trans_height, (1, 1, 1))  # Lower straight line
+    draw_line_mpl(20 + vehicle_trans_width, 110 + vehicle_trans_height, 20 + vehicle_trans_width, 120 + vehicle_trans_height, (1, 1, 1))  # Left attached line
+    draw_line_mpl(75 + vehicle_trans_width, 110 + vehicle_trans_height, 75 + vehicle_trans_width, 120 + vehicle_trans_height, (1, 1, 1))  # Right attached line
+    draw_line_mpl(20 + vehicle_trans_width, 120 + vehicle_trans_height, 30 + vehicle_trans_width, 120 + vehicle_trans_height, (1, 1, 1))  # Left attached line to the right
+    draw_line_mpl(30 + vehicle_trans_width, 120 + vehicle_trans_height, 30 + vehicle_trans_width, 130 + vehicle_trans_height, (1, 1, 1))  # To the right
+    draw_line_mpl(75 + vehicle_trans_width, 120 + vehicle_trans_height, 65 + vehicle_trans_width, 120 + vehicle_trans_height, (1, 1, 1))  # Right attached line to the left
+    draw_line_mpl(65 + vehicle_trans_width, 120 + vehicle_trans_height, 65 + vehicle_trans_width, 130 + vehicle_trans_height, (1, 1, 1))  # To the left
+    draw_line_mpl(30 + vehicle_trans_width, 130 + vehicle_trans_height, 65 + vehicle_trans_width, 130 + vehicle_trans_height, (1, 1, 1))  # Head
+
+    draw_line_mpl(car_bottom_left_x, car_bottom_left_y, car_top_left_x, car_top_left_y, (0, 0, 0))
+    draw_line_mpl(car_top_left_x, car_top_left_y, car_top_right_x, car_top_right_y, (0, 0, 0))
+    draw_line_mpl(car_bottom_right_x, car_bottom_right_y, car_top_right_x, car_top_right_y, (0, 0, 0))
+    draw_line_mpl(car_bottom_right_x, car_bottom_right_y, car_bottom_left_x, car_bottom_left_y, (0, 0, 0))
