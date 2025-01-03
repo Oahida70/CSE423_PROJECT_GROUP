39e5e63049d510d6ef16cd487a924f14d7d95298
Oahida70 <syada.oahida.yesmin@g.bracu.ac.bd>
2025-01-04 00:45:27 +0600

    Update ground.py

--- ground.py
+++ ground.py
@@ -1,7 +1,9 @@
 def ground():
-    
-    draw_line(0,100,800,100,(1,1,1))
-    for i in gravels:
+    draw_line_mpl(0, 100, 800, 100, (1, 1, 1))
+    for i in particles:
         glBegin(GL_POINTS)
-        glVertex2f(i[0],i[1])
+        glVertex2f(i[0], i[1])
         glEnd()
+
+for i in range(800):
+        particles.append((i, random.randint(92, 99)))
